40a858d43256d1d85df23801385ddd8e93805666
bananazigzags <bananazigzags@gmail.com>
2021-12-11 15:38:33 +0300

    changed status text logic

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ import data from "./data.js"
 function App() {
   const cards = data.map(event => 
   <Card 
+      key={event.id}
       img={event.coverImg}
       title={event.title}
       rating={event.stats.rating}
       reviewCount={event.stats.reviewCount}
       price={event.price}
       location={event.location}
-      status={event.openSpots > 0 ? `SPOTS LEFT: ${event.openSpots}` : "SOLD OUT"}
+      status={event.openSpots === 0 ? "SOLD OUT" : event.location === "Online" ? "ONLINE" : null}
   />)
   return (
     <div>
